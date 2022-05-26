dd680e283afc53797307ebf6bc60525b940703f4
djballowe <djballowe@gmail.com>
2022-05-26 15:01:55 -0700

    added exit button for form

--- resources/script.js
+++ resources/script.js
@@ -13,8 +13,13 @@ function addBook() {
 
 // pop up form for logging data
 
+
+
+
+
 function openForm() {
     document.getElementById('form').style.display = 'block';
+    console.log('working');
 }
 
 function closeForm() {
